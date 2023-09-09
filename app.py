e1023f9e3793dc3b3c1c7e94a56a0a94352b45e0
chiduanush <chiduanush1@gmail.com>
2023-09-09 10:44:03 +0530

    get data from spacex api

--- app.py
+++ app.py
@@ -10,5 +10,18 @@ def index():
     return render_template("index.html")
 
 
+def fetch_spacex_launches():
+    url = "https://api.spacexdata.com/v4/launches"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return []
+
+
+launches = fetch_spacex_launches()
+print(launches[0])
+
+
 if __name__ == "__main__":
     app.run(debug=True)
