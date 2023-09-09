6726c7a0c66e30468a0d5264c31448029ef3d028
chiduanush <chiduanush1@gmail.com>
2023-09-09 10:08:24 +0530

    get the initial flask app running

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+from datetime import datetime
+import requests
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "hello world!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
